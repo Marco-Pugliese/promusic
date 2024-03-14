08a3fe28913c25cbaa018274253751ab0d4950d5
Marco Pugliese <126952052+Marco-Pugliese@users.noreply.github.com>
2024-03-14 11:44:12 +0100

    updated bg homepage

--- src/Components/LandingPage/Hero/HeroSection.jsx
+++ src/Components/LandingPage/Hero/HeroSection.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 const HeroSection = () => {
-  const [heroHover, setHeroHover] = useState(false);
+  // const [heroHover, setHeroHover] = useState(false);
   return (
     <Container fluid className="p-0 m-0">
       <Row
-        onMouseEnter={() => {
-          setHeroHover(true);
-        }}
-        onMouseLeave={() => {
-          setHeroHover(false);
-        }}
+        // onMouseEnter={() => {
+        //   setHeroHover(true);
+        // }}
+        // onMouseLeave={() => {
+        //   setHeroHover(false);
+        // }}
         className="p-0 m-0"
       >
         <Col className="col-12 bg-proMusic"></Col>
@@ -20,7 +19,7 @@ const HeroSection = () => {
             id="heroFrase"
             className="w-100 h-100 d-flex align-items-center justify-content-center bg-filterdark text-center"
           >
-            "MUSIC IS THE ONLY REASON"
+            "MUSIC{"  "}IS{"  "}THE{"  "}ONLY{"  "}REASON"
           </div>
         </Col>
         <Col className="col-12 bg-proMusic2"></Col>
--- src/Components/LandingPage/Landingpage.jsx
+++ src/Components/LandingPage/Landingpage.jsx
@@ -1,23 +1,8 @@
-// import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import HeroSection from "./Hero/HeroSection";
 import Aside from "./Aside/Aside";
 
 const Landingpage = () => {
-  // const [scrollTop, setScrollTop] = useState(0);
-
-  // useEffect(() => {
-  //   const handleScroll = (event) => {
-  //     setScrollTop(window.scrollY);
-  //     console.log(window.scrollY);
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
   return (
     <>
       <Row id="hero" className="p-0 m-0 h-100 w-100 ">
@@ -26,7 +11,7 @@ const Landingpage = () => {
       <Row>
         <Col className="col-12 text-dark text-center border-bottom border-black pb-2 fs-2 ">
           <span className="border-top border-black pt-2 px-5 text-shadow">
-            SOMETHING
+            HAVE A LOOK
           </span>
         </Col>
         <Col className="col-12 px-0">
